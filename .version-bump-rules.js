de9109eb2cb0860554db4e5a569e5353b0573f3a
yaswanth6240 <yaswanth6240@gmail.com>
2024-03-07 18:57:19 +0530

    fix: try

--- .version-bump-rules.js
+++ .version-bump-rules.js
@@ -5,4 +5,16 @@ module.exports = {
       fixes: 1,
       other: 0,
     },
+    patch: {
+        breaking: 0,
+        features: 0,
+        fixes: 1,
+        other: 0,
+      },
+      major: {
+        breaking: 1,
+        features: 0,
+        fixes: 0,
+        other: 0,
+      },
   };

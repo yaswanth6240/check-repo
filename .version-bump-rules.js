55168bc18e17079b955a7be5b6d931cd7b60cb99
yaswanth6240 <yaswanth6240@gmail.com>
2024-03-08 03:06:29 +0530

    fix: try

--- .version-bump-rules.js
+++ .version-bump-rules.js
@@ -1,5 +1,29 @@
+
+const config = require('conventional-changelog-conventionalcommits')
+const conventionalCommitsConfig = await config({
+    'types': [
+      { type: 'build', hidden: true },
+      { type: 'release', hidden: true },
+      { type: 'deprecate', section: 'Deprecated' }, // minor
+      { type: 'devops', hidden: true },
+      { type: 'docs', hidden: true },
+      { type: 'feat', section: 'Added' }, // minor
+      { type: 'fix', section: 'Fixed' }, // patch
+      { type: 'perf', section: 'Performance' }, // patch
+      { type: 'refactor', hidden: true },
+      { type: 'remove', section: 'Removed' }, // major
+      { type: 'security', section: 'Security' }, // patch
+      { type: 'style', hidden: true },
+      { type: 'test', hidden: true },
+    ]
+  })
+
 module.exports = {
 
+    gitRawCommitsOpts: conventionalCommitsConfig.gitRawCommitsOpts,
+    parserOpts: conventionalCommitsConfig.parserOpts,
+    writerOpts: conventionalCommitsConfig.writerOpts,
+    conventionalChangelog: conventionalCommitsConfig.conventionalChangelog,
     recommendedBumpOpts: {
 
         whatBump (commits) {

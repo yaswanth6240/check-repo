63f426d0c8791e195c4ff54b2844c5ec07aef10c
yaswanth6240 <yaswanth6240@gmail.com>
2024-03-08 02:17:03 +0530

    fix: try

--- .version-bump-rules.js
+++ .version-bump-rules.js
@@ -1,40 +1,16 @@
-const config = require('conventional-changelog-conventionalcommits')
+module.exports = {recommendedBumpOpts: {
+      whatBump: (commits) => {
+        let level = 2
+        let breakings = 0
+        let features = 0
 
-module.exports = async () => {
-    const conventionalCommitsConfig = await config({
-      'types': [
-        { type: 'build', hidden: true },
-        { type: 'release', hidden: true },
-        { type: 'deprecate', section: 'Deprecated' }, // minor
-        { type: 'devops', hidden: true },
-        { type: 'docs', hidden: true },
-        { type: 'feat', section: 'Added' }, // minor
-        { type: 'fix', section: 'Fixed' }, // patch
-        { type: 'perf', section: 'Performance' }, // patch
-        { type: 'refactor', hidden: true },
-        { type: 'remove', section: 'Removed' }, // major
-        { type: 'security', section: 'Security' }, // patch
-        { type: 'style', hidden: true },
-        { type: 'test', hidden: true },
-      ]
-    })
-
-    console.log("The config " + config)
-  
-    return Object.assign(conventionalCommitsConfig, {
-      recommendedBumpOpts: {
-        whatBump: (commits) => {
-          let level = 2
-          let breakings = 0
-          let features = 0
-  
-          console.log("what bump called "+ commits)
-          commits.forEach(commit => {
+        commits.forEach(commit => {
             // adds additional breaking change notes
             // for the special case, test(system)!: hello world, where there is
             // a '!' but no 'BREAKING CHANGE' in body:
-  
+
             console.log("what bump called "+ commit)
+  
             if (commit.notes.length > 0) {
               breakings += commit.notes.length
               level = 0
@@ -49,52 +25,11 @@ module.exports = async () => {
             }
           })
   
-          return {
-            level: level,
-            reason: breakings === 1
-              ? `There is ${breakings} BREAKING CHANGE and ${features} features`
-              : `There are ${breakings} BREAKING CHANGES and ${features} features`
-          }
+        return {
+          level: level,
+          reason: breakings === 1
+            ? `There is ${breakings} BREAKING CHANGE and ${features} features`
+            : `There are ${breakings} BREAKING CHANGES and ${features} features`
         }
       }
-    })
-  }
-
-
-
-    // recommendedBumpOpts: {
-    //   whatBump: (commits) => {
-    //     let level = 2
-    //     let breakings = 0
-    //     let features = 0
-
-    //     commits.forEach(commit => {
-    //         // adds additional breaking change notes
-    //         // for the special case, test(system)!: hello world, where there is
-    //         // a '!' but no 'BREAKING CHANGE' in body:
-    //         addBangNotes(commit)
-
-    //         console.log("what bump called "+ commit)
-  
-    //         if (commit.notes.length > 0) {
-    //           breakings += commit.notes.length
-    //           level = 0
-    //         } else if (commit.type === 'feat' || commit.type === 'feature') {
-    //           features += 1
-    //           if (level === 2) {
-    //             level = 1
-    //           }
-    //         } else if (commit.type === 'remove') { // Your custom type
-    //           level = 0
-    //           breakings += 1
-    //         }
-    //       })
-  
-    //     return {
-    //       level: level,
-    //       reason: breakings === 1
-    //         ? `There is ${breakings} BREAKING CHANGE and ${features} features`
-    //         : `There are ${breakings} BREAKING CHANGES and ${features} features`
-    //     }
-    //   }
-    // }
+    }}

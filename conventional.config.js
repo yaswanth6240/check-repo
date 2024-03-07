e0ca189481da9b41286d78570335f78cd1c2e5b1
yaswanth6240 <yaswanth6240@gmail.com>
2024-03-07 17:45:49 +0530

    minor: release

--- conventional.config.js
+++ conventional.config.js
@@ -5,9 +5,6 @@ module.exports = {
       { filename: 'package-lock.json', type: 'json' },
     ],
     // Add your custom bump rules here
-    parserOpts: {
-      noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
-    },
     types: [
         { type: 'fix', section: 'Bug Fixes', hidden: false },
         { type: 'minor', section: 'Minor Changes', hidden: false },

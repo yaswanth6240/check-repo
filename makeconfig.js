b5343c6eaa79859bd5224ea5692022fcc3033354
yaswanth6240 <yaswanth6240@gmail.com>
2024-03-11 13:06:39 +0530

    fix: publish

--- makeconfig.js
+++
@@ -1,7 +0,0 @@
-const makeConfig = require('./version-bump-rules')
-
-makeConfig().then((data) => {
-    global.config = data
-    data.recommendedBumpOpts.whatBump([])
-    console.log("===> ", Date.now() ,  global.config)
-})
